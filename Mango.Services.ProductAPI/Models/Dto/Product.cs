f8939d82abf5b26c6490926b7ae8c4b2b8c06790
user <user@WIN-1R35BJ9G1FD>
2024-06-22 22:03:12 +0300

    Добавьте файлы проекта.

---
+++ Mango.Services.ProductAPI/Models/Dto/Product.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Mango.Services.ProductAPI.Models.Dto
+{
+    public class Product
+    {
+        [Key]
+        public int ProductId { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Range(1, 1000)]
+        public double Price { get; set; }
+        [AllowNull]
+        public string Description { get; set; }
+        [AllowNull]
+        public string CategoryName { get; set; }
+        [AllowNull]
+        public string ImageUrl { get; set; }
+
+    }
+}
